067d4d0cc3677131d6495059906450cd56f32809
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-06-05 23:33:29 +0900

    initalize gem

---
+++ lib/rubocop/exception_call.rb
@@ -0,0 +1,7 @@
+require "rubocop/exception_call/version"
+
+module Rubocop
+  module ExceptionCall
+    # Your code goes here...
+  end
+end
---
+++ lib/rubocop/exception_call/version.rb
@@ -0,0 +1,5 @@
+module Rubocop
+  module ExceptionCall
+    VERSION = "0.1.0"
+  end
+end
